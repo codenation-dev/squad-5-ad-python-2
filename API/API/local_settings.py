f0bd746361c06134b010daccc296126171444c76
Alã Damasceno <ala.damasceno@ssp.ba.gov.br>
2019-07-30 15:47:37 -0300

    Faltando configurações de email

---
+++ API/API/local_settings.py
@@ -0,0 +1,6 @@
+EMAIL_HOST = 'smtp.zoho.com'
+EMAIL_HOST_USER = 'alehfer@zoho.com'
+EMAIL_HOST_PASSWORD = '..xDYp45jgqnr.!'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
--- API/API/settings.py
+++ API/API/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-# from . import local_settings
+from . import local_settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
